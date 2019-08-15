8aa77cdef734ee44bdaf18390d6b761d9286d29d
bttodd11 <bttodd@icloud.com>
2019-08-14 22:08:37 -0500

    Updated Info

--- javascript.js
+++ javascript.js
@@ -1,10 +1,7 @@
-
-
 $(document).ready(function () {
 
 	var xmlhttp = new XMLHttpRequest();
 	const url = 'http://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code=%27mlb%27&all_star_sw=%27N%27&sort_order=name_asc&season=%272019%27';
-	const playerUrl = 'http://lookup-service-prod.mlb.com/json/named.roster_40.bam?team_id=%27121%27'
 	xmlhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
 			var myArr = JSON.parse(this.responseText);
@@ -25,10 +22,11 @@ $(document).ready(function () {
 		var i;
 		for (i = 0; i < arr.length; i++) {
 			if (arr[i].league == "AL") {
-				out += '<p>' + arr[i].name_display_long + '</p>' +
+				out += '<h4>' + arr[i].name_display_long + '</h4>' +
 					'<p>' + arr[i].league_full + '</p>' +
-					'<p>' + arr[i].venue_name +  '</p>' +
-					'<a href="' + arr[i].base_url + '">' + arr[i].base_url + '</a><br><br>' 
+					'<p>' + arr[i].venue_name + '</p>' +
+					'<h6>' + arr[i].city + "," + arr[i].state + '</h6>' +
+					'<a href="' + arr[i].website_url + '">' + arr[i].website_url + '</a><br><br>'
 			}
 		}
 		document.getElementById('alTeam').innerHTML = out;
@@ -39,22 +37,15 @@ $(document).ready(function () {
 		var i;
 		for (i = 0; i < arr.length; i++) {
 			if (arr[i].league == "NL")
-				out += '<p>' + arr[i].name_display_long + '</p>' +
+				out += '<h4>' + arr[i].name_display_long + '</h4>' +
 					'<p>' + arr[i].league_full + '</p>' +
-					 '<p>' + arr[i].venue_name + '</p>' +
-					 '<a href="' + arr[i].base_url + '">' + arr[i].base_url + '</a><br><br>' 
+					'<p>' + arr[i].venue_name + '</p>' +
+					'<h6>' + arr[i].city + ", " + arr[i].state + '</h6>' +
+					'<a href="' + arr[i].website_url + '">' + arr[i].website_url + '</a><br><br>'
 		}
 		document.getElementById('nlTeam').innerHTML = out;
 	}
 
-	function teamId(arr){
-		var out = "";
-		var t;
-		for (t = 0; i < arr.length; i++){
-			'http://lookup-service-prod.mlb.com/json/named.roster_40.bam?team_id=%27' + arr[t].team_id + '%27'
-		}
-
-	}
 
 })
 
